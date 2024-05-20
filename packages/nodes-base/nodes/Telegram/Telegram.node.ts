KarnOneOn24/n8n

commit 39ce501f41fa05fef2aee766d42283ab210385b5

feat(Telegram Node): Add support for local bot api server (#8437)
Co-authored-by: Jonathan Bennetts <jonathan.bennetts@gmail.com>

packages/nodes-base/nodes/Telegram/Telegram.node.ts
@@ -2067,7 +2067,7 @@ export class Telegram implements INodeType {
 							{
 								json: false,
 								encoding: null,
-								uri: `https://api.telegram.org/file/bot${credentials.accessToken}/${filePath}`,
+								uri: `${credentials.baseUrl}/bot${credentials.accessToken}/${filePath}`,
 								resolveWithFullResponse: true,
 								useStream: true,
 							},

packages/editor-ui/src/utils/testData/credentialTypeTestData.ts
@@ -26,10 +26,17 @@ export const credentialTypeTelegram = {
 			description:
 				'Chat with the <a href="https://telegram.me/botfather">bot father</a> to obtain the access token',
 		},
+		{
+			displayName: 'Base URL',
+			name: 'baseUrl',
+			type: 'string',
+			default: 'https://api.telegram.org',
+			description: 'Base URL for Telegram Bot API',
+		},
 	],
 	test: {
 		request: {
-			baseURL: '=https://api.telegram.org/bot{{$credentials.accessToken}}',
+			baseURL: '={{$credentials.baseUrl}}/bot{{$credentials.accessToken}}',
 			url: '/getMe',
 		},
 	},

packages/nodes-base/credentials/TelegramApi.credentials.ts
@@ -17,11 +17,18 @@ export class TelegramApi implements ICredentialType {
 			description:
 				'Chat with the <a href="https://telegram.me/botfather">bot father</a> to obtain the access token',
 		},
+		{
+			displayName: 'Base URL',
+			name: 'baseUrl',
+			type: 'string',
+			default: 'https://api.telegram.org',
+			description: 'Base URL for Telegram Bot API',
+		},
 	];
 
 	test: ICredentialTestRequest = {
 		request: {
-			baseURL: '=https://api.telegram.org/bot{{$credentials.accessToken}}',
+			baseURL: '={{$credentials.baseUrl}}/bot{{$credentials.accessToken}}',
 			url: '/getMe',
 		},
 	};

packages/nodes-base/nodes/Telegram/GenericFunctions.ts
@@ -202,7 +202,7 @@ export async function apiRequest(
 	const options: IRequestOptions = {
 		headers: {},
 		method,
-		uri: `https://api.telegram.org/bot${credentials.accessToken}/${endpoint}`,
+		uri: `${credentials.baseUrl}/bot${credentials.accessToken}/${endpoint}`,
 		body,
 		qs: query,
 		json: true,

packages/nodes-base/nodes/Telegram/TelegramTrigger.node.ts
@@ -293,7 +293,7 @@ export class TelegramTrigger implements INodeType {
 					{
 						json: false,
 						encoding: null,
-						uri: `https://api.telegram.org/file/bot${credentials.accessToken}/${file_path}`,
+						uri: `${credentials.baseUrl}/file/bot${credentials.accessToken}/${file_path}`,
 						resolveWithFullResponse: true,
 					},
 				);

commit 0fbdc382bf3f42d14726bedb74ade73236d30244

fix(Telegram Node): Revert typo introduced in #8437 (no-changelog) (#9472)

packages/nodes-base/nodes/Telegram/Telegram.node.ts
@@ -2067,7 +2067,7 @@ export class Telegram implements INodeType {
 							{
 								json: false,
 								encoding: null,
-								uri: `${credentials.baseUrl}/bot${credentials.accessToken}/${filePath}`,
+								uri: `${credentials.baseUrl}/file/bot${credentials.accessToken}/${filePath}`,
 								resolveWithFullResponse: true,
 								useStream: true,
 							},